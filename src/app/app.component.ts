c7ac575c085893941eb6078e57ce9653a57b97e0
Tang Tang <tang6723@hotmail.com>
2016-11-23 00:01:33 +0800

    Tang

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import {Platform, NavController} from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { TabsPage } from '../pages/tabs/tabs';
@@ -9,6 +9,7 @@ import { TabsPage } from '../pages/tabs/tabs';
   template: `<ion-nav [root]="rootPage"></ion-nav>`
 })
 export class MyApp {
+  nav: NavController;
   rootPage = TabsPage;
 
   constructor(platform: Platform) {
@@ -17,6 +18,39 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
+
+      //退出按钮重写******开始******
+      document.addEventListener('backbutton', () => {
+        let activeVC = this.nav.getActive();
+        let page = activeVC.instance;
+
+        if (!(page instanceof TabsPage)) {
+          if (!this.nav.canGoBack()) {
+            console.log('检测到在根视图点击了返回按钮。');
+            return platform.exitApp();
+          }
+          console.log('检测到在子路径中点击了返回按钮。');
+          return this.nav.pop();
+        }
+
+        // let tabs = page.tab;
+        // let activeNav = tabs.getSelected();
+        //
+        // if (!activeNav.canGoBack()) {
+        //   console.log('检测到在 tab 页面的顶层点击了返回按钮。');
+        //   return platform.exitApp();
+        // }
+        //
+        // console.log('检测到当前 tab 弹出层的情况下点击了返回按钮。');
+        // return activeNav.pop();
+
+      }, false);
+
+      //退出按钮重写******结束******
+
+
+
+
     });
   }
 }
